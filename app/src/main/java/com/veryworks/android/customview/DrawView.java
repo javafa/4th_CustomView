70c69b8d8b4f87c616a8a1be4c3518089d6147a0
javafa <nigne2016@gmail.com>
2017-09-18 15:37:14 +0900

    add Path

--- app/src/main/java/com/veryworks/android/customview/DrawView.java
+++ app/src/main/java/com/veryworks/android/customview/DrawView.java
@@ -4,13 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.ArrayList;
-
-import static android.R.attr.x;
-import static android.R.attr.y;
 
 /**
  * Created by pc on 9/18/2017.
@@ -18,17 +14,19 @@ import static android.R.attr.y;
 
 public class DrawView extends View {
     Paint paint;
-    // 원의 크기
-    float r = 10;
-    // 좌표값을 저장하는 저장소
-    ArrayList<Float> xs = new ArrayList<>();
-    ArrayList<Float> ys = new ArrayList<>();
+    Path currentPath;
+
     // 소스코드에서 사용하기 때문에 생성자 파라미터는 context만 필요
     public DrawView(Context context) {
         super(context);
         paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.MAGENTA);
+        paint.setStrokeWidth(5f);
+
+        currentPath = new Path();
     }
+
     // 화면을 그려주는 onDraw 오버라이드
     @Override
     protected void onDraw(Canvas canvas) {
@@ -36,19 +34,27 @@ public class DrawView extends View {
         // 1. 화면에 터치가 되면...
         // 2. 연속해서 그림을 그려준다
         // 2.1. 터치된 좌표에 작은 동그라미를 그려준다.
-        if(x > -1 && y > -1){
-            for(int i=0 ; i<xs.size() ; i++) {
-                canvas.drawCircle(xs.get(i), ys.get(i), r, paint);
-            }
-        }
+        canvas.drawPath(currentPath, paint);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        // 터치가 일어나면 좌표값을 세팅해준다.
-        xs.add(event.getX());
-        ys.add(event.getY());
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                // 이전점과 현재점 사이를 그리지 않는다
+                currentPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 이전점과 현재점 사이를 그린다.
+                currentPath.lineTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                // nothing to do
+                break;
+        }
+
 
         // onDraw 를 호출하는 메서드를 호출
         invalidate(); // <- 다른 언어에서는 대부분 그림을 그려주는 함수를 호출하는 메서드는
