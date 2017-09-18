9f80441a96c581004c3c48b40f65b4e6f035b6d7
javafa <nigne2016@gmail.com>
2017-09-18 16:46:13 +0900

    add PathTool

--- app/src/main/java/com/veryworks/android/customview/DrawView.java
+++ app/src/main/java/com/veryworks/android/customview/DrawView.java
@@ -8,6 +8,9 @@ import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pc on 9/18/2017.
  */
@@ -16,55 +19,61 @@ public class DrawView extends View {
     Paint paint;
     Path currentPath;
 
+    List<PathTool> paths = new ArrayList<>();
+
     // 소스코드에서 사용하기 때문에 생성자 파라미터는 context만 필요
     public DrawView(Context context) {
         super(context);
         paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(5f);
+        init();
+    }
 
-        currentPath = new Path();
+    private void init(){
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5f);
+        setColor(Color.BLACK);
     }
 
     public void setColor(int color){
-        paint.setColor(color);
+        PathTool tool = new PathTool();
+        tool.setColor(color);
+        currentPath = tool;
+        paths.add(tool);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                currentPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                currentPath.lineTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        invalidate();
+        return true;
     }
 
     // 화면을 그려주는 onDraw 오버라이드
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // 1. 화면에 터치가 되면...
-        // 2. 연속해서 그림을 그려준다
-        // 2.1. 터치된 좌표에 작은 동그라미를 그려준다.
-        canvas.drawPath(currentPath, paint);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-
-        switch(event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                // 이전점과 현재점 사이를 그리지 않는다
-                currentPath.moveTo(event.getX(), event.getY());
-                break;
-            case MotionEvent.ACTION_MOVE:
-                // 이전점과 현재점 사이를 그린다.
-                currentPath.lineTo(event.getX(), event.getY());
-                break;
-            case MotionEvent.ACTION_UP:
-                // nothing to do
-                break;
+        for(PathTool tool:paths) {
+            paint.setColor(tool.getColor());
+            canvas.drawPath(tool, paint);
         }
+    }
+}
 
-
-        // onDraw 를 호출하는 메서드를 호출
-        invalidate(); // <- 다른 언어에서는 대부분 그림을 그려주는 함수를 호출하는 메서드는
-                      //    기존 그림을 유지하는데, 안드로드는 지운다.
-        // 리턴이 false 일 경우는 touch 이벤트를 연속해서 발생시키지 않는다
-        // 즉, 드래그를 하면 onTouchEvent가 재 호출되지 않는다.
-        return true;
+class PathTool extends Path {
+    private int color;
+    public void setColor(int color){
+        this.color = color;
+    }
+    public int getColor(){
+        return this.color;
     }
 }
